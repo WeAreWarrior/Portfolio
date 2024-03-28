0911fa3624c4c348a1f270ac2548d913a515913c
WeAreWarrior <163636852+WeAreWarrior@users.noreply.github.com>
2024-03-28 20:37:23 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,255 @@
+<template>
+<div class="col-lg-12 fixed-top">
+  <div class="d-flex justify-content-center py-4">
+    <nav class="navbar navbar-expand-lg bg-dark d-none d-lg-block rounded-pill px-3">
+      <div class="container-fluid">
+        <a class="navbar-brand title text-white me-5" href="#">Catherine</a>
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-white fs-5" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-5" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-5" href="#skills">Skills</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-5" href="#projects">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-5" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+
+
+  <section id="home"></section>
+
+  <div class="container-fluid py-5">
+    <div class="row">
+      <div class="col-lg-6 py-4 d-none d-lg-block me-lg-4">
+        <img src="portrait.png" alt="Portrait" class="img-fluid round border-0">
+      </div>
+      <div class="col-lg-5 position-y ms-lg-5">
+        <p class="title t-text text-white">Catherine Dinoy</p>
+        <p class="header c-text text-white">Novice Programmer</p>
+        <h4 class="content mt-lg-5 text-white">"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do." </h4>
+      </div>
+    </div>
+  </div>
+
+  <section id="about"></section>
+
+  <h1 class="title text-white">About myself</h1>
+
+  <div class="container-fluid col-lg-11 py-5 px-4">
+    <div class="row">
+      <div class="col-lg-6 position-y">
+        <div class="row justify-content-start">
+          <div class="col-lg-8">
+            <div class="card bg-transparent border-0">
+              <div class="card-body">
+                <p class="content text-white fs-4">Hey there! My name is Catherine Dinoy. I am 23 years old. I am currently studying computer studies.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-end py-lg-5 py-md-3">
+          <div class="col-lg-8">
+            <div class="card bg-transparent border-0">
+              <div class="card-body">
+                  <p class="content text-white fs-4">Painting and drawing, listening to music, going on rides, and playing online games are my hobbies.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-start">
+          <div class="col-lg-8">
+            <div class="card bg-transparent border-0">
+              <div class="card-body">
+                <p class="content text-white fs-4">I enjoy Mobile Legends & Call Of Duty, cats, THE 100, anything Jollibee has to offer and dreaming of Paris.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <img src="about-img.png" width="100%" alt="About Portrait" class="img-fluid rounded-pill shadow">
+      </div>
+    </div>
+  </div>
+
+  <section id="skills"></section>
+
+  <h1 class="title text-white py-3">Skills I acquired</h1>
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-3 px-5">
+        <div class="col-lg-3 text-center py-4 my-3">
+            <img src="bootstrap.png" title="Bootstrap" class="zoom-in-effect">
+            <h2 class="content mt-3 text-white">Bootstrap</h2>
+        </div>
+        <div class="col-lg-3 text-center py-4 my-3">
+            <img src="css.png" title="CSS" class="zoom-in-effect">
+            <h2 class="content mt-3 text-white">CSS</h2>
+        </div>
+        <div class="col-lg-3 text-center py-4 my-3">
+            <img src="html.png" title="HTML" class="zoom-in-effect">
+            <h2 class="content mt-3 text-white">HTML</h2>
+        </div>
+        <div class="col-lg-3 text-center py-4 my-3">
+            <img src="vue.png" width="100%" title="Vue.js" class="zoom-in-effect">
+            <h2 class="content mt-2 text-white">Vue JS</h2>
+        </div>
+    </div>
+  </div>
+
+<section id="projects"></section>
+<h1 class="title py-3 text-white">Projects I created so far</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 py-4">
+      <div class="card text-bg-dark">
+        <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/434366758_375434778796524_4662397740624550953_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHnizdVpzmLHNKm0ExQfm374oO8loDPpkHig7yWgM-mQXtHsWcsNiUmX0dTl24_iyDoZgBFXqZYzl-tnclSUJFf&_nc_ohc=oTZJ3Wvi9CIAX8kO0Zo&_nc_ht=scontent-mnl1-1.xx&oh=00_AfAz2CuFmpAZQbVMfwYsOsyvBNSeNd3KPYR2s-xYWO3L9A&oe=66094DD1" class="card-img half-opacity" alt="...">
+        <div class="card-img-overlay">
+          <a href="" class="text-decoration-none">
+            <h5 class="card-title title c-text text-white position-y">Country Flags Information</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-12 py-4">
+      <div class="card text-bg-dark">
+        <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/434218168_375435902129745_7050265961982171715_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEv0CBrU04SUf6fmKGQoriSLcyaAYSq50ctzJoBhKrnRyhjtMt-MCSTwx9aCwYknJGJdeVvCr8X2o5oX84HwLM1&_nc_ohc=EgGInEwqZxUAX9pCJiT&_nc_ht=scontent-mnl1-1.xx&oh=00_AfBLx4gQADOR7yU5_MMA5OwrsHVjAkgghRHPXaQaRWnTMg&oe=660AE54A" class="card-img half-opacity" alt="...">
+        <div class="card-img-overlay">
+          <a href="" class="text-decoration-none">
+            <h5 class="card-title title c-text text-white position-y">Random Color Generator</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<section id="contact"></section>
+<h1 class="title py-3 text-white">Let's get to know more</h1>
+  <div class="container col-lg-8 pb-3">
+    <div class="row justify-content-center">
+      <div class="card">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3925.9535310313286!2d123.94856207479742!3d10.265327989854027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a99a424181dc51%3A0xc690858230d59842!2sCogon%20Elementary%20School!5e0!3m2!1sen!2sph!4v1711626136555!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="card-img-top py-2"></iframe>
+        <div class="card-body">
+          <h5 class="content">Adress</h5>
+        <a href="" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+         <i class="fa fa-map-marker"></i> Cogon, Cordova, Cebu  
+        </a>
+        <div class="bg-white p-4 rounded"> 
+            <h5 class="content">Call me</h5>
+              <a href="tel: +9307827093" class="d-inline-block text-decoration-none text-dark mb-2">
+                <i class="fa fa-mobile"></i> +9307827093</a>
+              <br>
+              <a href="tel: +9307827093" class="d-inline-block mb-2 text-decoration-none text-dark">
+                <i class="fa fa-mobile"></i> +9307827093</a>
+              <h5 class="mt-4 content">Email me</h5>
+              <a href=""  class="d-inline-block mb-2 text-decoration-none text-dark">
+                <i class="fa fa-envelope"></i> catherinedinoy@gmail.com  
+              </a>
+              <h5 class="mt-4 content">Follow Me</h5>
+                <a href="" class="d-inline-block text-dark fs-5 me-2">
+                  <i class="fab fa-facebook"></i>
+                </a>
+                <a href="" class="d-inline-block text-dark fs-5 me-2">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="d-inline-block text-dark fs-5 me-2">
+                  <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.half-opacity {
+  opacity: 0.5;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.t-text {
+  font-size: 120px;
+}
+
+.c-text {
+  font-size: 60px;
+}
+
+.position-y {
+  margin-top: 100px;
+}
+
+.title {
+  font-family: "Paprika", system-ui;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.content {
+  font-family: "Crimson Pro", serif;
+  font-optical-sizing: auto;
+  font-weight: 700;
+  font-style: normal;
+}
+
+.header {
+  font-family: "Permanent Marker", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+
+@media (max-width: 767px) {
+  .position-y {
+    margin-top: 0px;
+  }
+
+  .t-text {
+    font-size: 70px;
+  }
+
+  .c-text {
+    font-size: 40px;
+  }
+}
+
+</style>
